b62606c47bc62e7caa3501ce3c44a047940202dd
Gavindu Sajeendra <gavindu1995@gmail.com>
2026-01-01 23:36:45 +0530

    Inter Communication services added\

--- src/main/java/com/gusto/microservices/service/OrderService.java
+++ src/main/java/com/gusto/microservices/service/OrderService.java
@@ -1,11 +1,13 @@
 package com.gusto.microservices.service;
 
+import com.gusto.microservices.client.InventoryClient;
 import com.gusto.microservices.dto.OrderRequest;
 import com.gusto.microservices.model.Order;
 import com.gusto.microservices.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.management.RuntimeErrorException;
 import java.util.UUID;
 
 @Service
@@ -13,16 +15,27 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    public void placeOrder(OrderRequest orderRequest){
-        //map the order request to order object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuentity(orderRequest.quantity());
+    private final InventoryClient inventoryClient;
 
-        //save order to orderRepository
-        orderRepository.save(order);
+    public void placeOrder(OrderRequest orderRequest){
+        var isProductIsInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if(isProductIsInStock){
+            //map the order request to order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuentity(orderRequest.quantity());
+
+            //save order to orderRepository
+            orderRepository.save(order);
+
+        }else{
+            throw new RuntimeException("Present with skucode " + orderRequest.skuCode() + "Is Not in Stock");
+        }
+
+
 
 
     }
